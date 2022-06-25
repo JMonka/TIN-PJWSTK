66d5dd18028c0dbfb120c6e301e63e4f580f3d1a
JMonka <97990229+JMonka@users.noreply.github.com>
2022-06-25 09:38:42 +0200

    Add files via upload

---
+++ 02/2b.js
@@ -0,0 +1,8 @@
+let a = 1;
+let b = 2;
+
+if (a === b) {
+    console.log("Wyrazenia sa rowne");
+} else {
+    console.log("Wyrazenia sa nierowne");
+}
---
+++ 02/2c.js
@@ -0,0 +1,5 @@
+const brand = ["Nike", "Adidas", "Puma"];
+
+brand.forEach(element => {
+    console.log(element);
+});
